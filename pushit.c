7700124a02d6b2ee10c6bbdd954bf7a4d6736c03
Inoublii <53261561+Inoublii@users.noreply.github.com>
2020-01-06 00:27:11 +0100

    Update pushit.c

--- pushit.c
+++ pushit.c
@@ -7,19 +7,19 @@
 
 void pushit(stack_t **stack, unsigned int x)
 	{
-		stack_t *new = malloc(sizeof(stack_t));
+stack_t *new = malloc(sizeof(stack_t));
 char *arg = NULL;
 (void)x;
-		if (!new)
-		{
-			fprintf(stderr, "Error: malloc failed\n");
-			exit(EXIT_FAILURE);
+if (!new)
+	{
+	fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
 		}
-		new->n = atoi(arg);
-		new->next = (*stack);
-		new->prev = NULL;
+new->n = atoi(arg);
+	new->next = (*stack);
+new->prev = NULL;
 		if (*stack)
 			(*stack)->prev = new;
-		*stack = new;
+	*stack = new;
 	}
 
