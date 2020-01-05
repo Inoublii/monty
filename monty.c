ac5e7000620d6d3f576c68209f312a56a1ccd22b
Inoublii <53261561+Inoublii@users.noreply.github.com>
2020-01-06 00:25:47 +0100

    Update monty.c

--- monty.c
+++ monty.c
@@ -8,20 +8,20 @@
 
 int main(int argc, char **argv)
 {
-	stack_t **stack;
+stack_t **stack;
 
-	stack = malloc(sizeof(stack_t));
-	*stack = '\0';
-	(void)mighty;
-	if (argc >= 2 && argc <= 3)
+stack = malloc(sizeof(stack_t));
+*stack = '\0';
+(void)mighty;
+if (argc >= 2 && argc <= 3)
 		reads(argv[1], stack);
-	else
-	{
+else
+{
 		fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 	}
-	fm(stack);
-	free(stack);
-	return (0);
+fm(stack);
+free(stack);
+return (0);
 
 }
